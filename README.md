4b3653611cf01b19176de8d3cf0528e2ee9cc209
MasatoTakahashi <masato.takahahsi.2.71828@gmail.com>
2015-10-26 02:11:53 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# What's this?
+A script for batch processing for everyday's Trello operation.
+
+# Requirements
+Python 2.7.x
+
+# How to use
+1. make ** credential ** file in the same directory as these scripts
+1. get and write your credential key and access token etc. as followings
+```
+credential_key:xxxxxxxxxxxxx
+token:xxxxxxxxxxxxx
+board_name:project name
+username:yourname
+```
+1. just kick **.sh** file to execute each process.
+or directly type like
+```sh
+python trello_batch.py --mode archive
+```
+or
+```sh
+python trello_batch.py --mode create
+```
---
+++ trello_batch.py
@@ -0,0 +1,160 @@
+# coding: utf-8
+
+import os, sys, time
+import csv
+import urllib2
+import json
+from datetime import date
+import argparse
+
+class Trello_agent:
+    def __init__(self):
+        self.credential_key = ''
+        self.token = ''
+        self.board_name = ''
+        self.board_id = ''
+        self.username = ''
+        self.list_id = ''
+        self.done_cards = []
+    
+    def __str__(self):
+        return 'ckey:' + self.credential_key + \
+                '\ntoken:' + self.token + '\nboard_name:' + self.board_name + \
+                '\nboard_id:' + self.board_id + '\nlist_id:' + self.list_id
+        
+    def read_credential_file(self, filepath):
+        # try to read credential file        
+        try:
+            fn = open(filepath, 'r')
+        except IOError:
+            print 'Error: credential file does not exist'
+            sys.exit()
+        
+        for buf in fn.readlines():
+            k, v = buf.split(':')
+            if k == 'credential_key':
+                self.credential_key = v.strip()
+            elif k == 'token':
+                self.token = v.strip()
+            elif k == 'board_name':
+                self.board_name = v.strip()
+            elif k == 'username':
+                self.username = v.strip()
+            else:
+                print('following information is not valid')
+                print k + ':' + v
+
+    def get_board_id(self):
+        qstr = 'https://trello.com/1/members/' + self.username + '/boards?key=' + \
+                self.credential_key + '&token=' + self.token + '&fields=name'
+        try:
+            qres = urllib2.urlopen(qstr).read()
+        except:
+            print 'Access error'
+            sys.exit()
+        
+        # find specified board        
+        qres = json.loads(qres)
+        for name_id in qres:
+            if name_id['name'] == self.board_name:
+                self.board_id = name_id['id'].strip()
+
+    def get_list_id(self, list_name):
+        qstr = 'https://trello.com/1/boards/' + self.board_id + '/lists?key=' + \
+                self.credential_key + '&token=' + self.token + '&fields=name'
+        try:
+            qres = urllib2.urlopen(qstr).read()
+        except:
+            print 'Access error: could not get list id'
+            sys.exit()
+        
+        qres = json.loads(qres)
+        for name_id in qres:
+            if name_id['name'] == list_name:
+                self.list_id = name_id['id'].strip()        
+    
+    def get_done_cards(self):
+        qstr = 'https://api.trello.com/1/lists/' + self.list_id + \
+                '/cards?key=' + self.credential_key + '&token=' + self.token + \
+                '&filter=open'
+        
+        qres = urllib2.urlopen(qstr).read()
+        self.done_cards = json.loads(qres)
+    
+    def write_done_cards(self):
+        outfn = 'trello_done_log_' + time.strftime('%Y-%m-%d') + '.md'
+        fn = open(outfn, 'a+')
+        
+        for card in self.done_cards:
+            due = str(card['due'])
+            if due != 'None':
+                due = due[0:10].replace('-','/')
+            # for japanese board name, encode() is required 
+            write_str = '## ' + card['name'].encode('utf-8') + ' (' + due + ') \n\n\n'
+            fn.write(write_str)
+
+    def archive_all_dones(self):
+        qstr = 'https://api.trello.com/1/lists/' + self.list_id + \
+                '/archiveAllCards?key=' + self.credential_key + '&token=' + self.token 
+        
+        # generate POST 
+        req = urllib2.Request(qstr, '')
+        # archive all cards        
+        urllib2.urlopen(req)
+    
+    def batch_card_create(self, filepath):
+        # import schedule list
+        schedules = csv.reader(open(filepath, 'rb'), delimiter = ',')
+        
+        for row in schedules:
+            datebuf = row[1]
+            counter_datesep = datebuf.count('/')
+            error_flg = 0
+            if counter_datesep == 1:
+                datebuf = str(date.today().year) + '/' + datebuf
+            elif counter_datesep == 2:
+                datebuf
+            else:
+                error_flg = 1
+            
+            if error_flg == 0:
+                qstr = 'https://trello.com/1/cards?key=' + self.credential_key + \
+                        '&token=' + self.token + \
+                        '&idList=' + self.list_id + \
+                        '&name=' + row[0].replace(' ', '%20') + \
+                        '&due=' + datebuf
+                req = urllib2.Request(qstr, '')
+                urllib2.urlopen(req)
+            else:
+                print('Error: date column is not correct format')
+                print(row)
+        
+def make_log():
+    ai = Trello_agent()
+    ai.read_credential_file(os.getcwd() + '/credential')
+    ai.get_board_id()
+    ai.get_list_id('done')
+    ai.get_done_cards()
+    ai.write_done_cards()
+    ai.archive_all_dones()
+
+def card_batch_generate():
+    ai = Trello_agent()
+    ai.read_credential_file(os.getcwd() + '/credential')
+    ai.get_board_id()
+    ai.get_list_id('plan')
+    ai.batch_card_create(os.getcwd() + '/trello_list.csv')
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--mode', default = 'archive', help = 'archive or create')
+    args = parser.parse_args()
+
+    if args.mode == 'archive':
+        make_log()
+    elif args.mode == 'create':
+        card_batch_generate()
+    else:
+        print('Error: invalid mode is assigned')
+    
